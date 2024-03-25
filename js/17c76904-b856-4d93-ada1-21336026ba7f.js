2997d5d0410654bf1cb41af92b204e32f38b6722
Thiago <thiagofts@gmail.com>
2024-03-25 20:29:18 +0000

    Fix some links; Add basis for Images and meaning

---
+++ js/17c76904-b856-4d93-ada1-21336026ba7f.js
@@ -0,0 +1,47 @@
+function htLoadExercise() {
+    /*
+    $("#btncheck").val(keywords[29]);
+    $("#btnnew").val(keywords[30]);
+
+    for (let i = 0; i < 17; i += 2) {
+        $("#lblans"+i).text(keywords[31]);
+        $("#lblans"+(i+1)).text(keywords[32]);
+    }
+
+    for (let i = 0; i < 9; i++) {
+        $("#answer"+i).text("");
+        $("input[name=exercise"+i+"]").prop("checked", false);
+    }
+    */
+
+    return false;
+}
+
+/*
+function htCheckExercise(val0, val1, answer) {
+    var ans = parseInt($("input[name="+val0+"]:checked").val());
+    var text = "";
+    var format = "";
+    if (ans == val1) {
+        text = keywords[27];
+        format = "green";
+    } else {
+        text = keywords[28];
+        format = "red";
+    }
+    $(answer).text(text);
+    $(answer).css("color", format);
+
+    return false;
+}
+*/
+
+function htCheckAnswers()
+{
+    /*
+    var vector = [ 1, 0, 1, 0, 1, 1, 1, 1, 0];
+    for (let i = 0; i < vector.length; i++) {
+        htCheckExercise("exercise"+i, vector[i], "#answer"+i);
+    }
+    */
+}
