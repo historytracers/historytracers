a94444c44ae663b717b73ac3b3c9ab5f61f79f30
Thiago <thiagofts@gmail.com>
2024-03-28 01:19:14 +0000

    Imagens and meaning

--- js/17c76904-b856-4d93-ada1-21336026ba7f.js
+++ js/17c76904-b856-4d93-ada1-21336026ba7f.js
@@ -1,23 +1,93 @@
+function htPlotPoemChart(dest, yValue)
+{
+    var lang = htDetectLanguage();
+    var verse = (lang == "en-US" )? "verse" : "verso";
+    var syllable = (lang == "en-US" )? "syllable" : "sílaba";
+
+    const ctx = document.getElementById(dest).getContext("2d");
+    var chartId = new Chart(ctx, {
+         type: 'bubble',
+         data: {
+            labels: [verse+" 1", verse+" 2", verse+" 3", verse+" 4", verse+" 5"],
+            datasets: [{
+               data: [{
+                     x: 1,
+                     y: yValue,
+                     z: 20
+                  },
+                  {
+                     x: 2,
+                     y: yValue,
+                     z: 20
+                  },
+                  {
+                     x: 3,
+                     y: yValue,
+                     z: 20
+                  },
+                  {
+                     x: 4,
+                     y: yValue,
+                     z: 20
+                  },
+                  {
+                     x: 5,
+                     y: yValue,
+                     z: 20
+                  }
+               ],
+               backgroundColor: ['lightblue'],
+               borderColor: ['lightblue'],
+               radius: 8,
+            }],
+         },
+         options: {
+            responsive: false,
+            scales: {
+                y: {
+                    title: {
+                        display: true,
+                        text: syllable
+                    }
+                },
+                x: {
+                    beginAtZero: true,
+                    title: {
+                        display: true,
+                        text: verse
+                    }
+                }
+            },
+            plugins: {
+               legend: {
+                    display: false
+                }
+            } 
+         },
+    });
+}
+
 function htLoadExercise() {
-    /*
     $("#btncheck").val(keywords[29]);
     $("#btnnew").val(keywords[30]);
 
-    for (let i = 0; i < 17; i += 2) {
+    for (let i = 0; i < 14; i += 2) {
         $("#lblans"+i).text(keywords[31]);
         $("#lblans"+(i+1)).text(keywords[32]);
     }
 
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 7; i++) {
         $("#answer"+i).text("");
         $("input[name=exercise"+i+"]").prop("checked", false);
     }
-    */
+
+
+    htPlotPoemChart('chart0', 10);
+    htPlotPoemChart('chart1', 7);
 
     return false;
 }
 
-/*
 function htCheckExercise(val0, val1, answer) {
     var ans = parseInt($("input[name="+val0+"]:checked").val());
     var text = "";
@@ -34,14 +104,11 @@ function htCheckExercise(val0, val1, answer) {
 
     return false;
 }
-*/
 
 function htCheckAnswers()
 {
-    /*
-    var vector = [ 1, 0, 1, 0, 1, 1, 1, 1, 0];
+    var vector = [ 1, 1, 1, 1, 1, 1, 1 ];
     for (let i = 0; i < vector.length; i++) {
         htCheckExercise("exercise"+i, vector[i], "#answer"+i);
     }
-    */
 }
