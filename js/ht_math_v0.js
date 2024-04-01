8c14e384e217db08e1a43fbd9e47f6ae5c5365ed
Thiago <thiagofts@gmail.com>
2024-04-01 14:33:55 +0000

    Rename files; isolate chart from math

--- js/ht_math_v0.js
+++ js/ht_math_v0.js
@@ -1,8 +1,4 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-// https://stackoverflow.com/questions/11182924/how-to-check-if-javascript-object-is-json
-var mathStringConstructor = "gth".constructor;
-var mathVectorConstructor = [].constructor;
-var mathObjectConstructor = ({}).constructor;
 
 var yupanaSelectors = [ -1,  4,  3,  2,  4,  1,  1,  1, 1,  1,
                         -1, -1, -1, -1,  2, -1,  4,  3, 2,  2,
@@ -10,54 +6,6 @@ var yupanaSelectors = [ -1,  4,  3,  2,  4,  1,  1,  1, 1,  1,
 
 var yupanArr = [ ];
 
-// This function needs Chart JS to work
-function htPlotPoemChart(dest, yValue)
-{
-    var lang = htDetectLanguage();
-    var verse = (lang == "en-US" )? "verse" : "verso";
-    var syllable = (lang == "en-US" )? "syllable" : "sílaba";
-
-    var values = [];
-    if (yValue.constructor === mathVectorConstructor) {
-        for (let i = 0 ; i < yValue.length; i++) {
-            values.push( htPlotPoemChartElement(yValue[i]) );
-        }
-    } else {
-        values.push( htPlotPoemChartElement(yValue) );
-    }
-
-    const ctx = document.getElementById(dest).getContext("2d");
-    var chartId = new Chart(ctx, {
-         type: 'bubble',
-         data: {
-            labels: [verse+" 1", verse+" 2", verse+" 3", verse+" 4", verse+" 5"],
-            datasets: values,
-         },
-         options: {
-            responsive: false,
-            scales: {
-                y: {
-                    title: {
-                        display: true,
-                        text: syllable
-                    }
-                },
-                x: {
-                    beginAtZero: true,
-                    title: {
-                        display: true,
-                        text: verse
-                    }
-                }
-            },
-            plugins: {
-               legend: {
-                    display: false
-                }
-            } 
-         },
-    });
-}
 
 function htSplitDecimalDigit(output, maxIdx, value, base)
 {
