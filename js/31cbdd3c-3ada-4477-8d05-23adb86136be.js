2d06b6d7019b174eca318db7cec0590c0a1b9c12
Thiago <thiagofts@gmail.com>
2024-05-26 15:43:16 +0000

    Improve Chapter 1 'Introduction to groups'

--- js/31cbdd3c-3ada-4477-8d05-23adb86136be.js
+++ js/31cbdd3c-3ada-4477-8d05-23adb86136be.js
@@ -1,45 +1,24 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+var localAnswerVector31cbdd3c = undefined;
+
 function htLoadExercise() {
-    $("#btncheck").val(keywords[29]);
-    $("#btnnew").val(keywords[30]);
-
-    $("#lblans0").text(keywords[31]);
-    $("#lblans1").text(keywords[32]);
-    $("#lblans2").text(keywords[31]);
-    $("#lblans3").text(keywords[32]);
-    $("#lblans4").text(keywords[31]);
-    $("#lblans5").text(keywords[32]);
-
-    $("#answer0").text("");
-    $("#answer1").text("");
-    $("#answer2").text("");
-    $("input[name=exercise0]").prop("checked", false);
-    $("input[name=exercise1]").prop("checked", false);
-    $("input[name=exercise2]").prop("checked", false);
-
-    return false;
-}
-
-function htCheckExercise(val0, val1, answer) {
-    var ans = parseInt($("input[name="+val0+"]:checked").val());
-    var text = "";
-    var format = "";
-    if (ans == val1) {
-        text = keywords[27];
-        format = "green";
+    if (localAnswerVector31cbdd3c == undefined) {
+        localAnswerVector31cbdd3c = htLoadAnswersFromExercise();
     } else {
-        text = keywords[28];
-        format = "red";
+        htResetAnswers(localAnswerVector31cbdd3c);
     }
-    $(answer).text(text);
-    $(answer).css("color", format);
+
 
     return false;
 }
 
 function htCheckAnswers()
 {
-    htCheckExercise("exercise0", 0, "#answer0");
-    htCheckExercise("exercise1", 1, "#answer1");
-    htCheckExercise("exercise2", 0, "#answer2");
+    if (localAnswerVector31cbdd3c != undefined) {
+        for (let i = 0; i < localAnswerVector31cbdd3c.length; i++) {
+            htCheckExerciseAnswer("exercise"+i, localAnswerVector31cbdd3c[i], "#answer"+i, "#explanation"+i);
+        }
+    }
 }
+
