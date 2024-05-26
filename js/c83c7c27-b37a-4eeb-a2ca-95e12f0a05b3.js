656df2ee67acf337c5b598463f668d1a3dd0c787
Thiago <thiagofts@gmail.com>
2024-05-26 19:32:26 +0000

    Improve Chapter 1 'Groups of the life'

--- js/c83c7c27-b37a-4eeb-a2ca-95e12f0a05b3.js
+++ js/c83c7c27-b37a-4eeb-a2ca-95e12f0a05b3.js
@@ -1,45 +1,24 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+var localAnswerVectorc83c7c27 = undefined;
+
 function htLoadExercise() {
-    $("#btncheck").val(keywords[29]);
-    $("#btnnew").val(keywords[30]);
-
-    $("#lblans0").text(keywords[31]);
-    $("#lblans1").text(keywords[32]);
-    $("#lblans2").text(keywords[31]);
-    $("#lblans3").text(keywords[32]);
-    $("#lblans4").text(keywords[31]);
-    $("#lblans5").text(keywords[32]);
-
-    $("#answer0").text("");
-    $("#answer1").text("");
-    $("#answer2").text("");
-    $("input[name=exercise0]").prop("checked", false);
-    $("input[name=exercise1]").prop("checked", false);
-    $("input[name=exercise2]").prop("checked", false);
-
-    return false;
-}
-
-function htCheckExercise(val0, val1, answer) {
-    var ans = parseInt($("input[name="+val0+"]:checked").val());
-    var text = "";
-    var format = "";
-    if (ans == val1) {
-        text = keywords[27];
-        format = "green";
+    if (localAnswerVectorc83c7c27 == undefined) {
+        localAnswerVectorc83c7c27 = htLoadAnswersFromExercise();
     } else {
-        text = keywords[28];
-        format = "red";
+        htResetAnswers(localAnswerVectorc83c7c27);
     }
-    $(answer).text(text);
-    $(answer).css("color", format);
+
 
     return false;
 }
 
 function htCheckAnswers()
 {
-    htCheckExercise("exercise0", 0, "#answer0");
-    htCheckExercise("exercise1", 1, "#answer1");
-    htCheckExercise("exercise2", 1, "#answer2");
+    if (localAnswerVectorc83c7c27 != undefined) {
+        for (let i = 0; i < localAnswerVectorc83c7c27.length; i++) {
+            htCheckExerciseAnswer("exercise"+i, localAnswerVectorc83c7c27[i], "#answer"+i, "#explanation"+i);
+        }
+    }
 }
+
