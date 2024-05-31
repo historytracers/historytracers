c11580f6e8cb002ddfd08a5e0e021058a2c481d9
Thiago <thiagofts@gmail.com>
2024-05-31 17:28:43 +0000

    Chapter 2 'Understanding time' update

--- js/d7d99844-7df7-4327-86a0-920d5a5347b5.js
+++ js/d7d99844-7df7-4327-86a0-920d5a5347b5.js
@@ -1,24 +1,20 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-var d7d99844FirstTime = true;
+var FirstTimed7d99844 = true;
+var localAnswerVectord7d99844 = undefined;
+
 function htLoadExercise() {
-    $("#btncheck").val(keywords[29]);
-    $("#btnnew").val(keywords[30]);
-
-    for (let i = 0; i < 10; i += 2) {
-        $("#lblans"+i).text(keywords[31]);
-        $("#lblans"+(i+1)).text(keywords[32]);
+    if (localAnswerVectord7d99844 == undefined) {
+        localAnswerVectord7d99844 = htLoadAnswersFromExercise();
+    } else {
+        htResetAnswers(localAnswerVectord7d99844);
     }
 
-    for (let i = 0; i < 5; i++) {
-        $("#answer"+i).text("");
-        $("input[name=exercise"+i+"]").prop("checked", false);
-    }
     var current_time = Math.floor(Date.now()/1000);
     var local_lang = $("#site_language").val();
     var local_calendar = $("#site_calendar").val();
     var todayText = htConvertUnixDate(local_calendar, local_lang, current_time);
-    if (d7d99844FirstTime) {
+    if (FirstTimed7d99844) {
         var current_time = Math.floor(Date.now()/1000);
         var jd = calcUnixTime(current_time);
         var julianDays = gregorian_to_jd(jd[0], jd[1], jd[2]);
@@ -27,34 +23,19 @@ function htLoadExercise() {
 
         htFillMesoamericanCalendar(mesoamericanPeriod, 1);
 
-        d7d99844FirstTime = false;
+        FirstTimed7d99844 = false;
     }
     $("#htexampledate0").html(todayText);
 
     return false;
 }
 
-function htCheckExercise(val0, val1, answer) {
-    var ans = parseInt($("input[name="+val0+"]:checked").val());
-    var text = "";
-    var format = "";
-    if (ans == val1) {
-        text = keywords[27];
-        format = "green";
-    } else {
-        text = keywords[28];
-        format = "red";
-    }
-    $(answer).text(text);
-    $(answer).css("color", format);
-
-    return false;
-}
-
 function htCheckAnswers()
 {
-    var vector = [ 0, 1, 0, 1, 1 ];
-    for (let i = 0; i < vector.length; i++) {
-        htCheckExercise("exercise"+i, vector[i], "#answer"+i);
+    if (localAnswerVectord7d99844 != undefined) {
+        for (let i = 0; i < localAnswerVectord7d99844.length; i++) {
+            htCheckExerciseAnswer("exercise"+i, localAnswerVectord7d99844[i], "#answer"+i, "#explanation"+i);
+        }
     }
 }
+
