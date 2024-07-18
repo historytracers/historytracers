ad48752168f9c3bf9230f266cc6c2afb446d9e9a
Thiago <thiagofts@gmail.com>
2024-07-18 17:30:17 +0000

    Create a pattern for arrows (part I)

--- js/687ee328-19bb-4a65-ab46-7d707a2e11dc.js
+++ js/687ee328-19bb-4a65-ab46-7d707a2e11dc.js
@@ -7,7 +7,7 @@ function htLoadExercise() {
     localCounter687ee328 = htModifyArrow('.htUpArrow', localCounter687ee328);
     localCounter687ee328 = htModifyArrow('.htDownArrow', localCounter687ee328);
 
-    $(".htUpArrow").on("click", function() {
+    $("#traineeUp0").on("click", function() {
         localCounter687ee328++;
         localCounter687ee328 = htModifyArrow('.htUpArrow', localCounter687ee328);
         localCounter687ee328 = htModifyArrow('.htDownArrow', localCounter687ee328);
@@ -17,7 +17,7 @@ function htLoadExercise() {
         htFillYupanaDecimalValues('#yupana0', localCounter687ee328, 1, 'red_dot_right_up');
     });
 
-    $(".htDownArrow").on("click", function() {
+    $("#traineeDown0").on("click", function() {
         localCounter687ee328--;
         localCounter687ee328 = htModifyArrow('.htDownArrow', localCounter687ee328);
         localCounter687ee328 = htModifyArrow('.htUpArrow', localCounter687ee328);
@@ -30,8 +30,4 @@ function htLoadExercise() {
     return false;
 }
 
-function htCheckAnswers()
-{
-    return false;
-}
 
