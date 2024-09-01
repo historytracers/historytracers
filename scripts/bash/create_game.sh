de12afef9fda3ed49c908093eddb6b37f15285c7
Thiago <thiagofts@gmail.com>
2024-09-01 17:54:25 +0000

    Improve grammar of template desciptions

--- scripts/bash/create_game.sh
+++ scripts/bash/create_game.sh
@@ -14,7 +14,7 @@ ht_create_source_file () {
 ht_create_game_files () {
     CT=$(date +%s)
     cd ../../lang || exit
-    find ./* \( -name "??-??" \) -exec bash -c 'if [ ! -d "$1/smGame/" ]; then mkdir "$1/smGame/"; fi; cp ../src/json/scientific_method_game_template.json "$1/smGame/$2.json"; sed -i "s/File with all sources used in the text./$2/g" "$1/smGame/$2.json"; sed -i "s/Time in Unix Epoch showing last file update./$3/g" "$1/smGame/$2.json"' shell {} "$1" "$CT" \;
+    find ./* \( -name "??-??" \) -exec bash -c 'if [ ! -d "$1/smGame/" ]; then mkdir "$1/smGame/"; fi; cp ../src/json/scientific_method_game_template.json "$1/smGame/$2.json"; sed -i "s/A file containing all the sources referenced in the text./$2/g" "$1/smGame/$2.json"; sed -i "s/The time of the last file update, represented as Unix Epoch time./$3/g" "$1/smGame/$2.json"' shell {} "$1" "$CT" \;
 }
 
 ht_create_files() {
