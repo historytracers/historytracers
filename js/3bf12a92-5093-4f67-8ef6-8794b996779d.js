8848530f1a2e64ae3f737c5669e7fe78a6ca14d0
Thiago <thiagofts@gmail.com>
2024-09-15 23:30:14 +0000

    Add mathematics to Origin

---
+++ js/3bf12a92-5093-4f67-8ef6-8794b996779d.js
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+function htLoadExercise() {
+    htAddTreeReflection("#myFirstReflection", 55);
+    htAddTreeReflection("#GenealogicalZero", 73);
+
+    return false;
+}
+
