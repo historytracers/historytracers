1a97c6bfcd2cd315d01722acdb543e00d97ac80b
Thiago <thiagofts@gmail.com>
2024-09-17 04:26:15 +0000

    Refactor Origin (part III)

--- js/49c7f908-87cc-4fcc-8e4d-bcaba40e0aea.js
+++ js/49c7f908-87cc-4fcc-8e4d-bcaba40e0aea.js
@@ -1,23 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-var localAnswerVector49c7f908 = undefined;
-
 function htLoadExercise() {
-    if (localAnswerVector49c7f908 == undefined) {
-        localAnswerVector49c7f908 = htLoadAnswersFromExercise();
-    } else {
-        htResetAnswers(localAnswerVector49c7f908);
-    }
 
     return false;
-}
-
-function htCheckAnswers()
-{
-    if (localAnswerVector49c7f908 != undefined) {
-        for (let i = 0; i < localAnswerVector49c7f908.length; i++) {
-            htCheckExerciseAnswer("exercise"+i, localAnswerVector49c7f908[i], "#answer"+i, "#explanation"+i);
-        }
-    }
 }
 
