a932138326c886965a5b7153d2b31488f0ee2184
Thiago <thiagofts@gmail.com>
2024-09-23 16:50:43 +0000

    Start webserver adjusts allowing create content with it.

--- historytracers.py
+++ historytracers.py
@@ -1,20 +1,39 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 import http.server
+import json
 import socketserver
 
 PORT = 12345
+htOptions = None
 
 class htHandler(http.server.SimpleHTTPRequestHandler):
-    def _science(data):
-        print("Science content")
+    def _htJsonHeaders(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
 
-    def _historical(data):
-        print("Historical content")
+    def _htErrorHeaders(self):
+        self.send_response(404)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
 
-    def _firstSteps(data):
+    def _htScience(self):
+        self._htJson_headers(self)
+        self.wfile.write(json.dumps({'msg': 'Science content'}))
+
+    def _htHistorical(self):
+        self._htJsonHeaders(self)
+        self.wfile.write(json.dumps({'msg': 'Historical content'}))
+
+    def _htFirstSteps(self):
+        self._htJsonHeaders(self)
+        self.wfile.write(json.dumps({'msg': 'Historical content'}))
         print("First Steps content")
 
-    def _firstSteps(data):
-        print("Scientific Game content")
+    def _htSMGame(self):
+        self._htJsonHeaders(self)
+        self.wfile.write(json.dumps({'msg': 'Scientific Game content'}))
 
     def do_GET(self):
         http.server.SimpleHTTPRequestHandler.do_GET(self)
@@ -27,18 +46,42 @@ class htHandler(http.server.SimpleHTTPRequestHandler):
         listData = dataStr.split('&')
 
         if self.path == '/science':
-            _science(data)
+            self._htScience()
         elif self.path == '/history':
-            _historical(data)
+            self._htHistorical()
         elif self.path == '/first_steps':
-            _firstSteps(data)
+            self._htFirstSteps()
         elif self.path == '/smGame':
-            _smGame(data)
+            self._smGame()
 
         http.server.SimpleHTTPRequestHandler.do_GET(self)
 
-with socketserver.TCPServer(("", PORT), htHandler) as httpd:
-    string = "Access http://localhost:" + str(PORT)
-    print(string)
-    httpd.serve_forever()
+def parseArg():
+    options = None
+    try:
+        with open('.options.json', 'r') as file:
+            options = json.load(file)
+    except:
+        print("File '.options.json' not found. Server is going to run with defaults.")
+        options = {'devmode': False, 'port': 12345 }
 
+    print("Running with options: ")
+    print(options)
+    return options
+
+def runServer():
+    with socketserver.TCPServer(("", htOptions['port']), htHandler) as httpd:
+        string = "Access http://localhost:" + str(htOptions['port'])
+        print(string)
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            pass
+
+        httpd.server_close()
+
+
+if __name__ == "__main__":
+    htOptions = parseArg()
+
+    runServer()
