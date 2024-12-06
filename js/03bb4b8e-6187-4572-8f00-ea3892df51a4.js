85f633d0227fa1974e2c9d48f989b7c31c939895
History Tracers <historytracers@gmail.com>
2024-12-06 21:51:19 +0000

    Bigger or smaller

---
+++ js/03bb4b8e-6187-4572-8f00-ea3892df51a4.js
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+var localAnswerVector03bb4b8e = undefined;
+
+function htModify03bb4b8e(value)
+{
+    if (value < 1)
+        value = 1;
+    else if (value > 5)
+        value = 5;
+
+    return value;
+}
+
+function htLoadExercise() {
+    localCounter03bb4b8e = 1;
+    localCounter03bb4b8e = htModify03bb4b8e(localCounter03bb4b8e);
+
+    $("#traineeUp0").on("click", function() {
+        localCounter03bb4b8e++;
+        localCounter03bb4b8e = htModify03bb4b8e(localCounter03bb4b8e);
+
+        $('#leftHandImg').attr('src', 'images/'+(localCounter03bb4b8e - 1)+'Left_Hand_Small.png');
+        $('#rightHandImg').attr('src', 'images/'+localCounter03bb4b8e+'Right_Hand_Small.png');
+
+        $('#leftnum').html(localCounter03bb4b8e - 1);
+        $('#rightnum').html(localCounter03bb4b8e);
+    });
+
+    $("#traineeDown0").on("click", function() {
+        localCounter03bb4b8e--;
+        localCounter03bb4b8e = htModify03bb4b8e(localCounter03bb4b8e);
+
+        $('#leftHandImg').attr('src', 'images/'+(localCounter03bb4b8e - 1)+'Left_Hand_Small.png');
+        $('#rightHandImg').attr('src', 'images/'+localCounter03bb4b8e+'Right_Hand_Small.png');
+
+        $('#leftnum').html(localCounter03bb4b8e - 1);
+        $('#rightnum').html(localCounter03bb4b8e);
+    });
+
+    return false;
+}
+
