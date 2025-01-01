04f3d1069af31123e03970c856e500a21e17dab3
History Tracers <historytracers@gmail.com>
2025-01-01 00:01:16 +0000

    Gilgamesh context

---
+++ js/f899e6cf-790c-4fac-bded-4afacef3ec95.js
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+var localAnswerVectorf899e6cf = undefined;
+
+function htLoadExercise() {
+    if (localAnswerVectorf899e6cf == undefined) {
+        localAnswerVectorf899e6cf = htLoadAnswersFromExercise();
+    } else {
+        htResetAnswers(localAnswerVectorf899e6cf);
+    }
+
+    htAddReligionReflection("#htReligiousReflection");
+
+    return false;
+}
+
+function htCheckAnswers()
+{
+    if (localAnswerVectorf899e6cf != undefined) {
+        for (let i = 0; i < localAnswerVectorf899e6cf.length; i++) {
+            htCheckExerciseAnswer("exercise"+i, localAnswerVectorf899e6cf[i], "#answer"+i, "#explanation"+i);
+        }
+    }
+}
+
