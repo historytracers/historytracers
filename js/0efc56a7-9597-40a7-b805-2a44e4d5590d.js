09d5f9a1a3cdcb27f2217c4f364cb4cea150132b
History Tracers <historytracers@gmail.com>
2025-02-16 22:29:40 +0000

    Move All Copan images to directory

--- js/0efc56a7-9597-40a7-b805-2a44e4d5590d.js
+++ js/0efc56a7-9597-40a7-b805-2a44e4d5590d.js
@@ -29,7 +29,7 @@ function htAddAlterQImages(id)
     var kingOrder = [ "<i>Popol Hol</i> (2), <i>Yax K'uk' Mo'</i> (1),<br /> <i>Yax Pasaj Chan Yopaat</i> (16), <i>K'ahk' Yipyaj Chan K'awiil</i> (15)", "? (6), ? (5),<br /> <i>K'altuun Hix</i> (4), ? (3)", "<i>Moon Jaguar</i> (10), ? (9),<br /> <i>Wi' Yohl K'inich</i> (8), <i>Bahlam Nehn</i> (7)", "- <i>K'ahk' Joplaj Chan K'awiil</i> (14), <i>Waxaklajuun Ubaah K'awiil</i> (13),<br /> <i>K'ahk' Uti' Witz K'awiil</i> (12), <i>Butz' Chan</i> (11)"  ];
     $(id).html("");
     for (let i = 0; i < 4; i++) {
-        $(id).append("<div class=\"htSlide\"> <div class=\"htSlideCounter\">"+(i + 1)+" / 4</div> <img id=\"imgCopan"+i+"\" onclick=\"htImageZoom('imgCopan"+i+"', '0%')\" class=\"imgGameSize\" src=\"images/CopanAltarGenealogy"+i+".jpg\"><div class=\"htSlideCaption\">"+kingOrder[i]+"</div></div>");
+        $(id).append("<div class=\"htSlide\"> <div class=\"htSlideCounter\">"+(i + 1)+" / 4</div> <img id=\"imgCopan"+i+"\" onclick=\"htImageZoom('imgCopan"+i+"', '0%')\" class=\"imgGameSize\" src=\"images/Copan/CopanAltarGenealogy"+i+".jpg\"><div class=\"htSlideCaption\">"+kingOrder[i]+"</div></div>");
     }
     $(id).append("<i class=\"fa-solid fa-chevron-left htSlidePrev\" onclick=\"htPlusDivs(-1);\"></i> <i class=\"fa-solid fa-chevron-right htSlideNext\" onclick=\"htPlusDivs(1);\"></i>");
     htPlusDivs(0);
