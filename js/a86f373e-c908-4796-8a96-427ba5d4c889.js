1510d18f12ff2195ae600a199f02ea4813a31fab
History Tracers <historytracers@gmail.com>
2025-03-24 18:24:47 +0000

    Rewrite end of text

--- js/a86f373e-c908-4796-8a96-427ba5d4c889.js
+++ js/a86f373e-c908-4796-8a96-427ba5d4c889.js
@@ -1,23 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-var localAnswerVectora86f373e = undefined;
-
 function htLoadExercise() {
-    if (localAnswerVectora86f373e == undefined) {
-        localAnswerVectora86f373e = htLoadAnswersFromExercise();
-    } else {
-        htResetAnswers(localAnswerVectora86f373e);
-    }
-
     return false;
-}
-
-function htCheckAnswers()
-{
-    if (localAnswerVectora86f373e != undefined) {
-        for (let i = 0; i < localAnswerVectora86f373e.length; i++) {
-            htCheckExerciseAnswer("exercise"+i, localAnswerVectora86f373e[i], "#answer"+i, "#explanation"+i);
-        }
-    }
 }
 
