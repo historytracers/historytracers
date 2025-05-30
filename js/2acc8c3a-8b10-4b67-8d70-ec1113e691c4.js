663d3784d1305d85710b8c508720df11490f8342
History Tracers <historytracers@gmail.com>
2025-05-30 17:41:47 +0000

    mult_table: Multiply by Zero

--- js/2acc8c3a-8b10-4b67-8d70-ec1113e691c4.js
+++ js/2acc8c3a-8b10-4b67-8d70-ec1113e691c4.js
@@ -28,6 +28,42 @@ function htLoadExercise() {
 
     htWriteNavigation("first_steps");
 
+    var xVector1 = [ 0, 0.5,  1,  1.5, 2,  2.5, 3,  3.5, 4,  4.5, 5,  5.5, 6,  6.5, 7,  7.5, 8,  8.5, 9,  9.5, 10];
+    var yVector1 = [ 0, null, 0, null, 0, null, 0, null, 0, null, 0, null, 0, null, 0, null, 0, null, 0, null, 0];
+
+    var xVector2 = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var yVector2 = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    var chart1Options = {
+        "chartId" : "chart1",
+        "yVector" : yVector1,
+        "yLable": mathKeywords[14],
+        "yType" : "linear",
+        "xVector" : xVector1,
+        "xLable": mathKeywords[15],
+        "xType" : "linear",
+        "datasetFill" : false,
+        "ymin": 0,
+        "ymax": 10,
+        "useCallBack": false
+    };
+    htPlotConstantContinuousChart(chart1Options);
+
+    var chart2Options = {
+        "chartId" : "chart2",
+        "yVector" : yVector2,
+        "yLable": mathKeywords[14],
+        "yType" : "linear",
+        "xVector" : xVector2,
+        "xLable": mathKeywords[15],
+        "xType" : "linear",
+        "datasetFill" : false,
+        "ymin": 0,
+        "ymax": 10,
+        "useCallBack": false
+    };
+    htPlotConstantContinuousChart(chart2Options);
+
     return false;
 }
 
