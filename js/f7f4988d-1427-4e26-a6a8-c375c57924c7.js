051d3cceb0fe930826eddddc15afb7ea371d0dd5
History Tracers <historytracers@gmail.com>
2025-06-04 15:40:49 +0000

    table23: Begin new text about multiplication table

---
+++ js/f7f4988d-1427-4e26-a6a8-c375c57924c7.js
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+var localAnswerVector = undefined;
+
+function htLoadExercise() {
+    if (localAnswerVector == undefined) {
+        localAnswerVector = htLoadAnswersFromExercise();
+    } else {
+        htResetAnswers(localAnswerVector);
+    }
+
+    htWriteNavigation("first_steps");
+
+    return false;
+}
+
+function htCheckAnswers()
+{
+    if (localAnswerVector != undefined) {
+        for (let i = 0; i < localAnswerVector.length; i++) {
+            htCheckExerciseAnswer("exercise"+i, localAnswerVector[i], "#answer"+i, "#explanation"+i);
+        }
+    }
+}
+
