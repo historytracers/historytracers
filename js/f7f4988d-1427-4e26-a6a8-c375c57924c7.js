bf520fb41a54cc7bfb0c714982fa6a8d81a6ce87
History Tracers <historytracers@gmail.com>
2025-06-05 17:39:38 +0000

    table23: Triangle

--- js/f7f4988d-1427-4e26-a6a8-c375c57924c7.js
+++ js/f7f4988d-1427-4e26-a6a8-c375c57924c7.js
@@ -11,6 +11,24 @@ function htLoadExercise() {
 
     htWriteNavigation("first_steps");
 
+    var xVector1 = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var yVector1 = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    var chart1Options = {
+        "chartId" : "chart1",
+        "yVector" : yVector1,
+        "yLable": mathKeywords[14],
+        "yType" : "linear",
+        "xVector" : xVector1,
+        "xLable": mathKeywords[15],
+        "xType" : "linear",
+        "datasetFill" : true,
+        "ymin": 0,
+        "ymax": 10,
+        "useCallBack": false
+    };
+    htPlotConstantContinuousChart(chart1Options);
+
     return false;
 }
 
