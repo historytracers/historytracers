60b43020cefae5350a124ea52f41438f90ad1663
History Tracers <historytracers@gmail.com>
2025-06-12 23:01:01 +0000

    45table: Multiplying by five and Nature

--- js/ad9516ac-2893-4649-8d62-7f9cfe69cc64.js
+++ js/ad9516ac-2893-4649-8d62-7f9cfe69cc64.js
@@ -10,8 +10,12 @@ function htLoadExercise() {
     }
 
     var xVector =  [ 0, 1, 2,  3,  4,  5,  6,  7,  8,  9, 10];
+    var yVector0 = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    var yVector1 = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     var yVector2 = [ 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+    var yVector3 = [ 0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30];
     var yVector4 = [ 0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40];
+    var yVector5 = [ 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
 
     var chart1Options = {
         "datasets": [
@@ -56,6 +60,71 @@ function htLoadExercise() {
         "useCallBack": false
     };
     htPlotConstantContinuousChart(chart2Options);
+
+    var chart3Options = {
+        "datasets": [
+                    {
+                        data : yVector5,
+                        label : mathKeywords[16]+"5",
+                        fill : false
+                    }
+        ],
+        "chartId" : "chart3",
+        "yType" : "linear",
+        "xVector" : xVector,
+        "xLable": mathKeywords[15],
+        "xType" : "linear",
+        "datasetFill" : false,
+        "ymin": 0,
+        "ymax": 50,
+        "useCallBack": false
+    };
+    htPlotConstantContinuousChart(chart3Options);
+
+    var chart4Options = {
+        "datasets": [
+                    {
+                        data : yVector0,
+                        label : mathKeywords[16]+"0",
+                        fill : false
+                    },
+                    {
+                        data : yVector1,
+                        label : mathKeywords[16]+"1",
+                        fill : false
+                    },
+                    {
+                        data : yVector2,
+                        label : mathKeywords[16]+"2",
+                        fill : false
+                    },
+                    {
+                        data : yVector3,
+                        label : mathKeywords[16]+"3",
+                        fill : false
+                    },
+                    {
+                        data : yVector4,
+                        label : mathKeywords[16]+"4",
+                        fill : false
+                    },
+                    {
+                        data : yVector5,
+                        label : mathKeywords[16]+"5",
+                        fill : false
+                    }
+        ],
+        "chartId" : "chart4",
+        "yType" : "linear",
+        "xVector" : xVector,
+        "xLable": mathKeywords[15],
+        "xType" : "linear",
+        "datasetFill" : false,
+        "ymin": 0,
+        "ymax": 50,
+        "useCallBack": false
+    };
+    htPlotConstantContinuousChart(chart4Options);
 
     htWriteNavigation("first_steps");
 
