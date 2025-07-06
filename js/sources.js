030b0ad590239ee0446dd9d2de77115859fd4fd7
History Tracers <historytracers@gmail.com>
2025-07-06 02:37:12 +0000

    sourcesp2: Rework

--- js/sources.js
+++ js/sources.js
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-$(document).ready(function(){
-    htLoadPage('sources','json', '', false);
-});
+function htLoadExercise() {
+    $("#bndPortugal").html(keywords[88]);
 
+    return false;
+}
