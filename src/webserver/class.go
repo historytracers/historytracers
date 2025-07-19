da6274847aaf7920ae88db8958ecdf674b8a72e7
History Tracers <historytracers@gmail.com>
2025-07-19 22:32:23 +0000

    reading: Add Poem

--- src/webserver/class.go
+++ src/webserver/class.go
@@ -93,6 +93,8 @@ func htSetDefaultTemplateValues(fp *classTemplateFile, newFile string) {
 	fp.LastUpdate[0] = htUpdateTimestamp()
 	fp.Authors[0] = ""
 	fp.Reviewers[0] = ""
+	fp.Type = "class"
+	fp.Version = 2
 }
 
 func htWriteTemplateFile(lang string, newFile string, template *classTemplateFile) error {
