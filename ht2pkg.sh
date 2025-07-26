ffd22326658f92b3356179477ff2588e6b6a251b
History Tracers <historytracers@gmail.com>
2025-07-26 20:01:49 +0000

    atlas_audio1: Adjust files to use new directory

--- ht2pkg.sh
+++ ht2pkg.sh
@@ -38,7 +38,7 @@ ht_create_directories
 make
 
 # Run History Tracers
-./historytracers -minify -audiofiles -gedcom -verbose
+./historytracers -minify -audiofiles -gedcom -verbose -timestamp
 
 ht_copy_files
 
