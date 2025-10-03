76a32577a4c0dccfc0c3e0edc741ab4e4707b1f2
History Tracers <historytracers@gmail.com>
2025-10-03 03:34:17 +0000

    atlas_upd: Initial adjust to Atlas

--- js/atlas_images.js
+++ js/atlas_images.js
@@ -1,31 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 function htSelectAtlas(direction) {
-    var value = $("#atlasindex").find(":selected").val();
-    value = (value == undefined) ? 0 : parseInt(value);
-
-    var max = $("#atlasindex").find("option").length;
-    value += direction;
-    if (value < 1) {
-        value = 1;
-    } else if (value > max) {
-        value = max;
-    }
+    const pos = $("#atlasindex").find(":selected");
+    var value = (direction > 0) ? pos.next().val() : pos.prev().val();
 
     $("#atlasindex option[value="+value+"]").prop('selected', true);
-    htSelectAtlasMap(value - 1);
+    htSelectAtlasMap(value);
 
     var myURL = 'index.html?page=atlas&atlas_page='+value;
     $("#atlas").val(value);
     window.history.replaceState(null, null, myURL);
 }
 
-function htLoadExercise() {
+function htLoadContent() {
 
     $("#atlasindex").on( "change", function() {
-        var id = parseInt($(this).val());
-        var value = id - 1;
-        htSelectAtlasMap(value);
+        var id = $(this).val();
+        htSelectAtlasMap(id);
         var myURL = 'index.html?page=atlas&atlas_page='+id;
         $("#atlas").val(id);
         window.history.replaceState(null, null, myURL);
