723583b5df8181110ebfc6cdd7dcda84c48038fe
History Tracers <historytracers@gmail.com>
2025-11-02 03:24:44 +0000

    game_mult: Plot game

--- js/4f0474ba-3c05-43cd-8528-d3f383eae556.js
+++ js/4f0474ba-3c05-43cd-8528-d3f383eae556.js
@@ -2,27 +2,67 @@
 
 var localAnswerVector = undefined;
 
-function htLoadExercise() {
-    if (localAnswerVector == undefined) {
-        localAnswerVector = htLoadAnswersFromExercise();
-    } else {
-        htResetAnswers(localAnswerVector);
+var first = 0;
+var second = 0;
+var total = 0;
+
+var selector = 0;
+
+var stopMe = false;
+
+var chart = undefined;
+
+function htSequenceUpdateValue(add) {
+    if (stopMe) {
+        return;
+    }
+    selector += add ;
+
+    if (selector < 0) {
+        selector = 0;
+    } else if ( selector > 10) {
+        selector = 10;
     }
 
-    return false;
+    const test = (first == 0 || second == 0 ) ? 0 : selector * first;
+    var result = "";
+    if (test < 10) {
+        result += "&nbsp;";
+    }
+    result += test;
+
+    if (test) {
+        htDrawMultiplicationTable("#visual", first, selector);
+    }
+    if (test == total) {
+        result += "<br /><i class=\"fa-solid fa-chevron-right\" style=\"font-size:1.0em;\" onclick=\"htFillExercise();\"></i>";
+        stopMe = true;
+    }
+    $("#finalResult").html(result);
+
 }
 
-function htCheckAnswers()
-{
-    if (localAnswerVector != undefined) {
-        for (let i = 0; i < localAnswerVector.length; i++) {
-            htCheckExerciseAnswer("exercise"+i, localAnswerVector[i], "#answer"+i, "#explanation"+i);
-        }
+function htFillExercise() {
+    first = htGetRandomArbitrary(0, 9);
+    second = htGetRandomArbitrary(0, 9);
+    total = first * second;
+    selector = 0;
+
+    if (chart) {
+        chart.destroy();
     }
+
+    $("#exercise").html("<spam class=\"text_to_paint\">&nbsp;&nbsp;&nbsp;"+first+"</spam><br /><spam class=\"text_to_paint\">× "+second+"</spam>");
+    chart = htFillMultiplicationTable("chart1", first, first, false, false);
+    $("#finalResult").html("");
+    htResetMultiplicationTable("#visual");
+    stopMe = false;
 }
 
 function htLoadContent() {
     htWriteNavigation();
+
+    htFillExercise();
 
     return false;
 }
