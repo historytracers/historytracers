da549818662904f10b49b4b6ada15b46e2c76703
History Tracers <historytracers@gmail.com>
2025-11-03 14:18:42 +0000

    rest: First commit about rest

---
+++ js/ebb5e822-8c85-41ee-92b0-6b6b82adf573.js
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+var localAnswerVector = undefined;
+
+function htLoadExercise() {
+    if (localAnswerVector == undefined) {
+        localAnswerVector = htLoadAnswersFromExercise();
+    } else {
+        htResetAnswers(localAnswerVector);
+    }
+
+    return false;
+}
+
+function htCheckAnswers()
+{
+    if (localAnswerVector != undefined) {
+        for (let i = 0; i < localAnswerVector.length; i++) {
+            htCheckExerciseAnswer("exercise"+i, localAnswerVector[i], "#answer"+i, "#explanation"+i);
+        }
+    }
+}
+
+function htLoadContent() {
+    htWriteNavigation();
+
+    htWriteMultiplicationTable("#mParent4", 4);
+
+    return false;
+}
