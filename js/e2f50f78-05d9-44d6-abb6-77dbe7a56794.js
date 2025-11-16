952c7ca392e5ee3b21375c93c62ccd5d7f79ea9f
History Tracers <historytracers@gmail.com>
2025-11-16 02:52:18 +0000

    game: Add images and basis

--- js/e2f50f78-05d9-44d6-abb6-77dbe7a56794.js
+++ js/e2f50f78-05d9-44d6-abb6-77dbe7a56794.js
@@ -1,28 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-var localAnswerVector = undefined;
+var localGameVectore2f50f78 = [];
+var currentLevel = 0;
 
-function htLoadExercise() {
-    if (localAnswerVector == undefined) {
-        localAnswerVector = htLoadAnswersFromExercise();
-    } else {
-        htResetAnswers(localAnswerVector);
-    }
-
-    return false;
-}
-
-function htCheckAnswers()
-{
-    if (localAnswerVector != undefined) {
-        for (let i = 0; i < localAnswerVector.length; i++) {
-            htCheckExerciseAnswer("exercise"+i, localAnswerVector[i], "#answer"+i, "#explanation"+i);
-        }
+function htFillImage() {
+    var obj = localGameVectore2f50f78[currentLevel];
+    $("#imgGame").attr("src", obj.imagePath);
+    $("#imgText").html(obj.imageDesc);
+    currentLevel++;
+    if (currentLevel == localGameVectore2f50f78.length) {
+        currentLevel = 0;
     }
 }
 
 function htLoadContent() {
     htWriteNavigation();
+
+    localGameVectore2f50f78 = htLoadGameData();
+    htFillImage();
 
     return false;
 }
