5ec2181780af3173d5ef8eb18a444aaf07a6fac2
History Tracers <historytracers@gmail.com>
2025-12-24 15:18:29 +0000

    div_app: Adjusts (It has different bugs)

--- js/9e2e82c6-804c-4541-80b4-7df337d32764.js
+++ js/9e2e82c6-804c-4541-80b4-7df337d32764.js
@@ -1,25 +1,25 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-//         "additionalInfo": "À medida que diminuímos a quantidade final de tortilhas, estamos fazendo tortilhas maiores, pois todas possuem a mesma quantidade inicial de massa. Dessa forma, 320 dividido por 2 é igual a 160, que por sua vez é o dobro do tamanho obtido quando dividimos por 4, o qual é o dobro de 2.<br />Além disso, ressaltamos que, pela primeira vez, efetuamos a divisão das unidades, pois elas não foram utilizadas junto com as dezenas.<br />Por último, destacamos que, como temos resto zero, isso indica que 320 é múltiplo de 2, assim como de 4 e de 5.<br /><p><table style=\"width: 30%; margin-left: auto; margin-right: auto; font-weight: none; border-collapse: collapse;\"><tr><td>&nbsp; 320 </td><td style=\"border-bottom: 2px solid black; border-left: 2px solid black;\"> 2 </td></tr><tr><td style=\"border-bottom: 2px solid black;\">- 200 </td><td>160</td></tr><tr><td>&nbsp; 120 </td><td> &nbsp; </td></tr><tr><td style=\"border-bottom: 2px solid black;\">- 120 </td><td> &nbsp; </td></tr> <tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 0</td><td> &nbsp; </td></tr> <tr><td style=\"border-bottom: 2px solid black;\">- &nbsp;&nbsp;&nbsp; 0 </td><td> &nbsp; </td></tr>  <tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 0</td><td> &nbsp; </td></tr></table></p>"
-
 var working = 0;
 var stopValue = 0;
 var workingValue = 0;
 
 var divisor = 1;
-var use = "";
+var usingValue = "";
 var idx = 2;
 var results = "";
 
 var dividend = 0;
 var strDividendValue = "";
+var stop = true;
 
 function htUpdateView(end) {
     var localUse = divisor * workingValue;
 
     var sign = "&nbsp; ";
     if (end) {
-        for (let i = localUse.toString().length; i < strDividendValue.length; i++) {
+        var loopEnd = working.toString().length;
+        for (let i = localUse.toString().length; i < loopEnd; i++) {
             localUse *= 10;
         }
         sign = "- ";
@@ -28,17 +28,34 @@ function htUpdateView(end) {
     $("#tc1f"+idx).html(sign+""+localUse);
     if (end) {
         var res = idx + 1;
-        $("#tc1f"+res).html("&nbsp; "+(dividend - localUse));
+        working = dividend - localUse;
+
+        if (working < divisor) {
+            stop = true;
+        }
+        $("#tc1f"+res).html("&nbsp; "+working);
     }
 }
 
 function htMoveDivAhead() {
     results = results + stopValue.toString();
+    strDividendValue = working.toString();
+
+    // Check length also
+    usingValue = (strDividendValue.length > 2 && parseInt(strDividendValue[0]) < divisor) ? strDividendValue[0]+""+strDividendValue[1] : strDividendValue[0];
+    stopValue = parseInt(parseInt(usingValue) / divisor);
+
+    workingValue = 0;
+
     idx += 2;
 }
 
 function htDivisionUpdateValue(n)
 {
+    if (stop) {
+        return false;
+    }
+
     if (workingValue == stopValue) {
         htUpdateView(true);
         htMoveDivAhead();
@@ -52,7 +69,6 @@ function htDivisionUpdateValue(n)
         workingValue = 0;
     }
 
-    
     $("#tc2f2").html(results+workingValue);
 
     if (workingValue == stopValue) {
@@ -67,22 +83,37 @@ function htDivisionUpdateValue(n)
 }
 
 function htNewDivision() {
+    if (!stop) {
+        return false;
+    }
+    results = "";
     workingValue = 0;
-    working = dividend = htGetRandomArbitrary(10, 999);
+    idx = 2;
+    working = dividend = htGetRandomArbitrary(10, 99);
+    //working = dividend = htGetRandomArbitrary(10, 999);
     strDividendValue = dividend.toString();
 
     divisor = htGetRandomArbitrary(1, 9);
 
-    use = (parseInt(strDividendValue[0]) < divisor) ? strDividendValue[0]+""+strDividendValue[1] : strDividendValue[0];
-    stopValue = parseInt(parseInt(use) / divisor);
+    usingValue = (strDividendValue.length > 2 && parseInt(strDividendValue[0]) < divisor) ? strDividendValue[0]+""+strDividendValue[1] : strDividendValue[0];
+    stopValue = parseInt(parseInt(usingValue) / divisor);
 
-    $("#tc1fd1").html("&nbsp; "+dividend);
+    for (let j = 2; j < 7; j++) {
+        for (let i = 1; i < 2; i++) {
+            $("#tc"+i+"f"+j).html("&nbsp;");
+        }
+    }
+
+    $("#tc1fd1").html("&nbsp; "+working);
     $("#tc2fds1").html("&nbsp; "+divisor);
 
     $("#tc2f2").html(workingValue);
 
+    $("#tc1f8").html(mathKeywords[35]+" <b>"+usingValue+" ÷ "+divisor+"</b><br />"+mathKeywords[36]);
 
-    $("#tc1f8").html(mathKeywords[35]+" <b>"+use+" ÷ "+divisor+"</b><br />"+mathKeywords[36]);
+    stop = false;
+
+    return false;
 }
 
 function htDivisionAddCommonTable(id) {
