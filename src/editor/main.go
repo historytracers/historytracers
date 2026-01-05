f31fe552c9d47324d9efea15e459cffd261a7198
History Tracers <historytracers@gmail.com>
2026-01-05 00:06:18 +0000

    editing: Insert audio

--- src/editor/main.go
+++ src/editor/main.go
@@ -164,6 +164,7 @@ func (e *TextEditor) createMenu() {
 	)
 
 	insertMenu := fyne.NewMenu("Insert",
+		fyne.NewMenuItem("Audio", e.insertAudio),
 		fyne.NewMenuItem("Date", e.insertDate),
 		fyne.NewMenuItem("Source", e.insertSource),
 		fyne.NewMenuItem("Text", e.insertText),
@@ -1204,6 +1205,51 @@ func (e *TextEditor) isInsideSourceArray() (bool, int) {
 	return false, -1
 }
 
+func (e *TextEditor) isInsideAudioArray() (bool, int) {
+	if e.currentDoc == nil {
+		return false, -1
+	}
+	content := e.currentDoc.content
+	fullText := content.Text
+
+	lines := strings.Split(fullText, "\n")
+	if content.CursorRow >= len(lines) {
+		return false, -1
+	}
+	cursorPos := 0
+	for i := 0; i < content.CursorRow; i++ {
+		cursorPos += len(lines[i]) + 1
+	}
+	cursorPos += content.CursorColumn
+
+	re := regexp.MustCompile(`"(?:audio)"\s*:\s*\[`)
+	matches := re.FindAllStringIndex(fullText, -1)
+
+	for _, match := range matches {
+		startIndex := match[1] - 1
+
+		openCount := 0
+		endIndex := -1
+		for i := startIndex + 1; i < len(fullText); i++ {
+			if fullText[i] == '[' {
+				openCount++
+			} else if fullText[i] == ']' {
+				if openCount == 0 {
+					endIndex = i
+					break
+				}
+				openCount--
+			}
+		}
+
+		if endIndex != -1 && cursorPos > startIndex && cursorPos <= endIndex {
+			return true, cursorPos
+		}
+	}
+
+	return false, -1
+}
+
 func (e *TextEditor) isInsideDateArray() (bool, int) {
 	if e.currentDoc == nil {
 		return false, -1
@@ -1247,6 +1293,70 @@ func (e *TextEditor) isInsideDateArray() (bool, int) {
 	}
 
 	return false, -1
+}
+
+func (e *TextEditor) insertAudio() {
+
+	isInside, cursorPos := e.isInsideAudioArray()
+
+	if !isInside {
+
+		dialog.ShowError(fmt.Errorf("cursor must be inside a \"audio\" JSON array"), e.window)
+
+		return
+
+	}
+
+	if e.currentDoc == nil {
+
+		return
+
+	}
+
+	audio := HTAudio{
+		URL:      "https://www.historytracers.org/audios/",
+		External: true,
+		Spotify:  false,
+	}
+
+	jsonData, err := json.MarshalIndent(audio, "", "  ")
+
+	if err != nil {
+
+		dialog.ShowError(err, e.window)
+
+		return
+
+	}
+
+	textBefore := e.currentDoc.content.Text[:cursorPos]
+
+	trimmedTextBefore := strings.TrimRight(textBefore, " \t\n")
+
+	insertText := string(jsonData)
+
+	if len(trimmedTextBefore) > 0 {
+
+		lastChar := trimmedTextBefore[len(trimmedTextBefore)-1]
+
+		if lastChar != '[' && lastChar != ',' {
+
+			insertText = ",\n" + insertText
+
+		}
+
+	}
+
+	content := e.currentDoc.content
+
+	oldClipboard := e.window.Clipboard().Content()
+
+	e.window.Clipboard().SetContent(insertText)
+
+	content.TypedShortcut(&fyne.ShortcutPaste{Clipboard: e.window.Clipboard()})
+
+	e.window.Clipboard().SetContent(oldClipboard)
+
 }
 
 func (e *TextEditor) insertDate() {
