e31b6428f4b266f1adf5f4e6a4cf7e904837fd42
History Tracers <historytracers@gmail.com>
2026-01-29 03:12:35 +0000

    grouping: Real

--- js/3c038244-e808-4ad2-bc2a-4663cbfeae3d.js
+++ js/3c038244-e808-4ad2-bc2a-4663cbfeae3d.js
@@ -24,27 +24,8 @@ function htCheckAnswers()
 function htLoadContent() {
     htWriteNavigation();
 
-    var xVector1 = [ 0, 0.5,  1,  1.5, 2,  2.5, 3,  3.5, 4,  4.5, 5,  5.5, 6,  6.5, 7,  7.5, 8,  8.5, 9,  9.5, 10];
-    var yVector3 = [ 0, null, 1, null, 2, null, 3, null, 4, null, 5, null, 6, null, 7, null, 8, null, 9, null, 10];
-
-    var chart3Options = {
-        "datasets": [
-                    {
-                        data : yVector3,
-                        label : mathKeywords[16]+"1",
-                        fill : false
-                    }],
-        "chartId" : "chart3",
-        "yType" : "linear",
-        "xVector" : xVector1,
-        "xLable": mathKeywords[15],
-        "xType" : "linear",
-        "ymin": 0,
-        "ymax": 10,
-        "useCallBack": false
-    };
-    htPlotConstantContinuousChart(chart3Options);
-
+    htFillMultiplicationTable("chart3", 1, 1, false, false);
+    htFillMultiplicationTable("chart4", 1, 1, false, true);
 
     return false;
 }
