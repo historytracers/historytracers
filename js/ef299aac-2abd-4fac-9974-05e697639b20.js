b6b8ca1954ea362746d427e216159603ab69c1c7
History Tracers <historytracers@gmail.com>
2026-03-11 00:13:37 +0000

    walking: Game (p1)

--- js/ef299aac-2abd-4fac-9974-05e697639b20.js
+++ js/ef299aac-2abd-4fac-9974-05e697639b20.js
@@ -2,25 +2,6 @@
 
 var localAnswerVector = undefined;
 
-function htLoadExercise() {
-    if (localAnswerVector == undefined) {
-        localAnswerVector = htLoadAnswersFromExercise();
-    } else {
-        htResetAnswers(localAnswerVector);
-    }
-
-    return false;
-}
-
-function htCheckAnswers()
-{
-    if (localAnswerVector != undefined) {
-        for (let i = 0; i < localAnswerVector.length; i++) {
-            htCheckExerciseAnswer("exercise"+i, localAnswerVector[i], "#answer"+i, "#explanation"+i);
-        }
-    }
-}
-
 function htLoadContent() {
     htWriteNavigation();
 
