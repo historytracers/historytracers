d318b8af0d6dd86d02e102083de373929473e03a
History Tracers <historytracers@gmail.com>
2026-04-28 15:36:34 +0000

    slope: Negative multiplication

--- js/61ebf26a-c8ab-4bb4-bd1a-5bd2d5a5a28a.js
+++ js/61ebf26a-c8ab-4bb4-bd1a-5bd2d5a5a28a.js
@@ -21,8 +21,30 @@ function htCheckAnswers()
     }
 }
 
+function htSetMyLocalTable(val) {
+    htWriteMultiplicationTable("#mParent1", val);
+    htWriteMultiplicationTable("#mParent2", -1*val);
+}
+
 function htLoadContent() {
     htWriteNavigation();
+
+    for (let i = 1; i < 10; i++) {
+        $('#tableValue').append($('<option>', {
+            value: i,
+            text: i
+        }));
+    }
+
+    $('#tableValue').val(2);
+    htSetMyLocalTable(2);
+
+    $('#tableValue').change(function() {
+        var selectedValue = $(this).val();
+        $("#mParent1").html("");
+        $("#mParent2").html("");
+        htSetMyLocalTable(selectedValue);
+    });
 
     return false;
 }
