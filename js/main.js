833c655aed169802af42867eb4491d5095701998
History Tracers <historytracers@gmail.com>
2026-04-29 14:15:32 +0000

    images: Add new function to index

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 function htLoadContent() {
+    htSetImageSrc("originmigration", "images/Mapswire/mapswire-world-political-white-equal_earth_journey.png");
+
     var divRadius = parseInt($(".htCircle[name='fig1']").width());
 
     $(".htCircle").mouseenter(function(){
