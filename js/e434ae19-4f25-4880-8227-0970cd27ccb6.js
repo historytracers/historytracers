7451b3186ed866f3fb9b7d32b4359d8ff5d35cce
History Tracers <historytracers@gmail.com>
2026-05-05 19:37:30 +0000

    sorobn: Indigenous (P1)

--- js/e434ae19-4f25-4880-8227-0970cd27ccb6.js
+++ js/e434ae19-4f25-4880-8227-0970cd27ccb6.js
@@ -1,32 +1,14 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-var localAnswerVector = undefined;
-
-function htLoadExercise() {
-    if (localAnswerVector == undefined) {
-        localAnswerVector = htLoadAnswersFromExercise();
-    } else {
-        htResetAnswers(localAnswerVector);
-    }
-
-    return false;
-}
-
-function htCheckAnswers()
-{
-    if (localAnswerVector != undefined) {
-        for (let i = 0; i < localAnswerVector.length; i++) {
-            htCheckExerciseAnswer("exercise"+i, localAnswerVector[i], "#answer"+i, "#explanation"+i);
-        }
-    }
-}
+var localCountere434ae19 = 0;
 
 function htAddElementTo687ee328Table(tableID, rowID, rows)
 {
     var left = 0;
     var right = 0;
+    var htImgSrcPrefix = htGetImgSrcPrefix();
     for (let i = 0 ; i < rows ; i++) {
-        $(tableID+" "+rowID+i).html("<td><img id=\"idf"+i+"\" onclick=\"htImageZoom('idf"+i+"', '0%')\" src=\"images/HistoryTracers/"+left+"Left_Hand_Small.png\" /></td><td><img id=\"ids"+i+"\" onclick=\"htImageZoom('ids"+i+"', '0%')\" src=\"images/HistoryTracers/"+right+"Right_Hand_Small.png\" /></td><td><img id=\"idt"+i+"\" onclick=\"htImageZoom('idt"+i+"', '0%')\" src=\"images/HistoryTracers/Maya_"+i+".png\" /></td>");
+        $(tableID+" "+rowID+i).html("<td><img id=\"idf"+i+"\" onclick=\"htImageZoom('idf"+i+"', '0%')\" src=\"images/HistoryTracers/"+left+"Left_Hand_Small.png\" /></td><td><img id=\"ids"+i+"\" onclick=\"htImageZoom('ids"+i+"', '0%')\" src=\"images/HistoryTracers/"+right+"Right_Hand_Small.png\" /></td><td><img id=\"idt"+i+"\" onclick=\"htImageZoom('idt"+i+"', '0%')\" src=\""+htImgSrcPrefix+"images/HistoryTracers/Maya_"+i+".png\" /></td><td><span class=\"text_to_paint\">"+i+"</span></td>");
         if (i < 5) {
             right += 1;
         } else {
@@ -42,30 +24,30 @@ function htLoadContent() {
 
     htSetImageSrc("imgTohoku", "images/Tohoku/Soroban.png");
 
-    localCounter687ee328 = 0;
-    localCounter687ee328 = htModifyArrow('.htUpArrow', localCounter687ee328);
-    localCounter687ee328 = htModifyArrow('.htDownArrow', localCounter687ee328);
+    localCountere434ae19 = 0;
+    localCountere434ae19 = htModifyArrow('.htUpArrow', localCountere434ae19);
+    localCountere434ae19 = htModifyArrow('.htDownArrow', localCountere434ae19);
 
     $("#traineeUp0").on("click", function() {
-        localCounter687ee328++;
-        localCounter687ee328 = htModifyArrow('.htUpArrow', localCounter687ee328);
-        localCounter687ee328 = htModifyArrow('.htDownArrow', localCounter687ee328);
+        localCountere434ae19++;
+        localCountere434ae19 = htModifyArrow('.htUpArrow', localCountere434ae19);
+        localCountere434ae19 = htModifyArrow('.htDownArrow', localCountere434ae19);
 
-        htSetImageForMembers('#leftHandImg', 'Left_Hand_Small.png', '#rightHandImg', 'Right_Hand_Small.png', localCounter687ee328);
+        htSetImageForMembers('#leftHandImg', 'Left_Hand_Small.png', '#rightHandImg', 'Right_Hand_Small.png', localCountere434ae19);
         htCleanYupanaDecimalValues('#yupana0', 5);
-        htFillYupanaDecimalValues('#yupana0', localCounter687ee328, 1, 'red_dot_right_up');
+        htFillYupanaDecimalValues('#yupana0', localCountere434ae19, 1, 'red_dot_right_up');
     });
 
     $("#traineeDown0").on("click", function() {
-        localCounter687ee328--;
-        localCounter687ee328 = htModifyArrow('.htDownArrow', localCounter687ee328);
-        localCounter687ee328 = htModifyArrow('.htUpArrow', localCounter687ee328);
+        localCountere434ae19--;
+        localCountere434ae19 = htModifyArrow('.htDownArrow', localCountere434ae19);
+        localCountere434ae19 = htModifyArrow('.htUpArrow', localCountere434ae19);
 
-        htSetImageForMembers('#leftHandImg', 'Left_Hand_Small.png', '#rightHandImg', 'Right_Hand_Small.png', localCounter687ee328);
+        htSetImageForMembers('#leftHandImg', 'Left_Hand_Small.png', '#rightHandImg', 'Right_Hand_Small.png', localCountere434ae19);
         htCleanYupanaDecimalValues('#yupana0', 5);
-        htFillYupanaDecimalValues('#yupana0', localCounter687ee328, 1, 'red_dot_right_up');
+        htFillYupanaDecimalValues('#yupana0', localCountere434ae19, 1, 'red_dot_right_up');
     });
-    htAddElementTo687ee328Table("#tblHandsCounting", "#tr", 11);
+    htAddElementTo687ee328Table("#tblHandsCounting", "#tr", 10);
 
     return false;
 }
