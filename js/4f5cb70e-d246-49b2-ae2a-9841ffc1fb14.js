2ae3304d29e40f6ee4cacad47d6fe50b89859a52
History Tracers <historytracers@gmail.com>
2026-05-27 15:29:54 +0000

    upd_atlas: Equality (P1)

--- js/4f5cb70e-d246-49b2-ae2a-9841ffc1fb14.js
+++ js/4f5cb70e-d246-49b2-ae2a-9841ffc1fb14.js
@@ -1,6 +1,44 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+var localCounter4f5cb = 0;
+
+function htModify4f5cb(value)
+{
+    if (value < 0)
+        value = 0;
+    else if (value > 5)
+        value = 5;
+
+    return value;
+}
+
 function htLoadContent() {
+    localCounter4f5cb = 0;
+    localCounter4f5cb = htModify4f5cb(localCounter4f5cb);
+
+    $("#traineeUp0").on("click", function() {
+        localCounter4f5cb++;
+        localCounter4f5cb = htModify4f5cb(localCounter4f5cb);
+
+        var prefix = htGetImgSrcPrefix();
+        $('#leftHandImg').attr('src', prefix+'images/HistoryTracers/'+localCounter4f5cb+'Left_Hand_Small.png');
+        $('#rightHandImg').attr('src', prefix+'images/HistoryTracers/'+localCounter4f5cb+'Right_Hand_Small.png');
+
+        $('#leftnum').html(localCounter4f5cb);
+        $('#rightnum').html(localCounter4f5cb);
+    });
+
+    $("#traineeDown0").on("click", function() {
+        localCounter4f5cb--;
+        localCounter4f5cb = htModify4f5cb(localCounter4f5cb);
+
+        var prefix = htGetImgSrcPrefix();
+        $('#leftHandImg').attr('src', prefix+'images/HistoryTracers/'+localCounter4f5cb+'Left_Hand_Small.png');
+        $('#rightHandImg').attr('src', prefix+'images/HistoryTracers/'+localCounter4f5cb+'Right_Hand_Small.png');
+
+        $('#leftnum').html(localCounter4f5cb);
+        $('#rightnum').html(localCounter4f5cb);
+    });
     htWriteNavigation();
 
     return false;
