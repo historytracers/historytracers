8a94b63d8c08077cfb5a92c296bd30b22f4b397a
History Tracers <historytracers@gmail.com>
2026-05-31 17:45:08 +0000

    sum_abacus: Sum 0 to 4 (P1)

--- js/ba340a77-cf71-4471-8e4c-43afa358833b.js
+++ js/ba340a77-cf71-4471-8e4c-43afa358833b.js
@@ -1,33 +1,42 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-var localAnswerVector = undefined;
-
-function htLoadExercise() {
-    if (localAnswerVector == undefined) {
-        localAnswerVector = htLoadAnswersFromExercise();
-    } else {
-        htResetAnswers(localAnswerVector);
-    }
-
-    return false;
-}
-
-function htCheckAnswers()
-{
-    if (localAnswerVector != undefined) {
-        for (let i = 0; i < localAnswerVector.length; i++) {
-            htCheckExerciseAnswer("exercise"+i, localAnswerVector[i], "#answer"+i, "#explanation"+i);
-        }
-    }
-}
+var localCounterba340a77 = 0;
 
 function htLoadContent() {
     htWriteNavigation();
+
+    localCounterba340a77 = 0;
+    localCounterba340a77 = htModifyArrow('.htUpArrow', localCounterba340a77);
+    localCounterba340a77 = htModifyArrow('.htDownArrow', localCounterba340a77);
+
+    $("#traineeUp0").on("click", function() {
+        localCounterba340a77++;
+        localCounterba340a77 = htModifyArrow('.htUpArrow', localCounterba340a77);
+        localCounterba340a77 = htModifyArrow('.htDownArrow', localCounterba340a77);
+
+        htSetImageForMembers('#leftHandImg', 'Left_Hand_Small.png', '#rightHandImg', 'Right_Hand_Small.png', localCounterba340a77);
+        htCleanYupanaDecimalValues('#yupana0', 5);
+        htFillYupanaDecimalValues('#yupana0', localCounterba340a77, 1, 'red_dot_right_up');
+    });
+
+    $("#traineeDown0").on("click", function() {
+        localCounterba340a77--;
+        localCounterba340a77 = htModifyArrow('.htDownArrow', localCounterba340a77);
+        localCounterba340a77 = htModifyArrow('.htUpArrow', localCounterba340a77);
+
+        htSetImageForMembers('#leftHandImg', 'Left_Hand_Small.png', '#rightHandImg', 'Right_Hand_Small.png', localCounterba340a77);
+        htCleanYupanaDecimalValues('#yupana0', 5);
+        htFillYupanaDecimalValues('#yupana0', localCounterba340a77, 1, 'red_dot_right_up');
+    });
+
     $("#htChinaZhongguo").html(keywords[137]);
     $("#htJapanNipponNihonKoku").html(keywords[139]);
 
     htSetImageSrc("imgSoroban", "images/Tohoku/Soroban.png");
     htSetImageSrc("imgSuanpan", "images/BritishMuseum/mid_01381495_001.jpg");
+
+    htSetImageSrc("leftHandImg", "images/HistoryTracers/0Left_Hand_Small.png");
+    htSetImageSrc("rightHandImg", "images/HistoryTracers/0Right_Hand_Small.png");
 
     return false;
 }
