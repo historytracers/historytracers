8ed28449c6d9cf62203d81c611bf358a2b9f9a03
History Tracers <historytracers@gmail.com>
2026-05-31 12:32:05 -0300

    img_upd4: SMGame

---
+++ js/f866455d-713d-4e3c-b25a-c151f1a05296.js
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+function htLoadContent() {
+    var localKeysVector = undefined;
+    htSetImageSrc("imgLL", "images/Archive/LucreciaLeme.png");
+    htSetImageSrc("imgPD", "images/Archive/PedroDias.png");
+    htSetImageSrc("imgMMF", "images/HistoryTracers/MusaMusaFortun.png");
+    return false;
+}
---
+++ js/f952fb69-fcf9-4980-9b31-38be2ecfe971.js
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+function htLoadContent() {
+    var localKeysVector = undefined;
+    htSetImageSrc("imgANTTINCOMPLETE", "images/ANTT/PT-TT-MCO-A-C-002-007-0006-00066_m0001_derivada.jpg");
+    htSetImageSrc("PDPFamilyTree", "images/ANTT/ANTT_PT-TT-TSO-CG-A-008-001-23165_m0062_derivada.jpg");
+    return false;
+}
