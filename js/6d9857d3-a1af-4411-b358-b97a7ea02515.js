711619c7c2b9afeb3e4700374ea5cae2a2fbd68e
History Tracers <historytracers@gmail.com>
2026-06-04 03:05:20 +0000

    sum_soroban2: Introduction

---
+++ js/6d9857d3-a1af-4411-b358-b97a7ea02515.js
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+function htLoadContent() {
+    htWriteNavigation();
+    $("#htChinaZhongguo").html(keywords[137]);
+    $("#htJapanNipponNihonKoku").html(keywords[139]);
+
+    return false;
+}
