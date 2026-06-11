0f7fa869b3ab9ed8ff0df85363506ab07122618e
History Tracers <historytracers@gmail.com>
2026-06-11 15:30:15 +0000

    fix_mention: Fix sources

--- js/sources.js
+++ js/sources.js
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 function htLoadContent() {
-    $("#bndPortugal").html(keywords[88]);
-
     htSetImageSrc("bndPortugal6r", "images/BibliotecaNacionalDigital/BNDTordesillas_6r.jpg");
     htSetImageSrc("imgKL", "images/Ashmolean/KingList.jpg");
 
