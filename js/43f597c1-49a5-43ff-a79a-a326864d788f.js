02db21da5dce81857aa53be19e74aaea82f8080c
History Tracers <historytracers@gmail.com>
2026-06-16 02:31:07 +0000

    new_year: Introduction

--- js/43f597c1-49a5-43ff-a79a-a326864d788f.js
+++ js/43f597c1-49a5-43ff-a79a-a326864d788f.js
@@ -23,6 +23,7 @@ function htCheckAnswers()
 
 function htLoadContent() {
     htWriteNavigation();
+    htSetImageSrc('imgUniverseTimeBigBang', 'images/ESA/The_Universe_across_space_and_time.jpg');
 
     return false;
 }
