d19b7ff0193871b2a0752679bc6f0f7cee87862a
History Tracers <historytracers@gmail.com>
2026-06-20 14:16:14 +0000

    yupana_games: Adjust text and yupana

--- js/b0bb8da3-ca00-453e-9060-9dfa767c80e2.js
+++ js/b0bb8da3-ca00-453e-9060-9dfa767c80e2.js
@@ -4,11 +4,12 @@ var local = {};
 
 function htFillCurrentYupanaSum()
 {
-    local.lvalues = htFillYupanaDecimalValues('#yupana1', $('#ia2yupana0').val(), 5, 'red_dot_right_up');
-    local.rvalues = htFillYupanaDecimalValues('#yupana1', $('#ia2yupana1').val(), 5, 'blue_dot_right_bottom');
+    local.lvalues = htFillYupanaDecimalValues('#yupana1', $('#ia2yupana0').val(), 4, 'red_dot_right_up');
+    local.rvalues = htFillYupanaDecimalValues('#yupana1', $('#ia2yupana1').val(), 4, 'blue_dot_right_bottom');
     if (local.sumFirstTime) {
         htWriteYupanaValuesOnHTMLTable('#vl', '#yupana1', local.lvalues);
         htWriteYupanaValuesOnHTMLTable('#vr', '#yupana1', local.rvalues);
+        local.sumFirstTime = false;
     }
 }
 
@@ -17,42 +18,43 @@ function htLoadContent() {
 
     local = { "lvalues": [], "rvalues": [], "sumFirstTime": true, "counter": 0, "answerVector": undefined }; 
 
-    if (local.sumFirstTime) {
-        $( "#ia2yupana1" ).bind( "keyup mouseup", function() {
-            $("input[name='yupanaradio']").prop("checked", false);
-            var value = $(this).val();
-            if (value < 0 || value > 99999) {
+    $( "#ia2yupana1" ).bind( "keyup mouseup", function() {
+        $("input[name='yupanaradio']").prop("checked", false);
+        var value = $(this).val();
+        if (value < 0) {
                 $(this).val(0);
-            }
-        });
+        } else if (value > 4999) {
+            $(this).val(4999);
+        }
+    });
 
-        $( "#ia2yupana1" ).bind( "keyup mouseup", function() {
-            $("input[name='yupanaradio']").prop("checked", false);
-            var value = $(this).val();
-            if (value < 0 || value > 99999) {
-                $(this).val(0);
-            }
-        });
-        htFillCurrentYupanaSum();
-        local.sumFirstTime = false;
-    }
+    $( "#ia2yupana1" ).bind( "keyup mouseup", function() {
+        $("input[name='yupanaradio']").prop("checked", false);
+        var value = $(this).val();
+        if (value < 0) {
+            $(this).val(0);
+        } else if (value > 4999) {
+            $(this).val(4999);
+        }
+    });
+    htFillCurrentYupanaSum();
 
     $( "input[name='yupanaradio']" ).on( "change", function() {
         var value = $(this).val();
-        htCleanYupanaDecimalValues('#yupana1', 5);
+        htCleanYupanaDecimalValues('#yupana1', 4);
 
         htFillCurrentYupanaSum();
         if (value == "values") {
-            htCleanYupanaAdditionalColumn('#yupana1', 5, '#tc6f');
+            htCleanYupanaAdditionalColumn('#yupana1', 4, '#tc6f');
             $('#tc7f1').html("");
             htWriteYupanaValuesOnHTMLTable('#vl', '#yupana1', local.lvalues);
             htWriteYupanaValuesOnHTMLTable('#vr', '#yupana1', local.rvalues);
         } else {
             var totals = htSumYupanaVectors(local.lvalues, local.rvalues);
-            htCleanYupanaDecimalValues('#yupana1', 5);
-            htFillYupanaDecimalValues('#yupana1', totals, 5, 'red_dot_right_up');
+            htCleanYupanaDecimalValues('#yupana1', 4);
+            htFillYupanaDecimalValues('#yupana1', totals, 4, 'red_dot_right_up');
             htWriteYupanaValuesOnHTMLTable('#tc6f', '#yupana1', totals);
-            htWriteYupanaSumMovement(local.lvalues, local.rvalues, '#yupana1', 5, '#tc7f1');
+            htWriteYupanaSumMovement(local.lvalues, local.rvalues, '#yupana1', 4, '#tc7f1');
         }
     });
 
