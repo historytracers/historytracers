c06d3d8745581e74c247ea53f913227726b92cb1
thiagoftsm <thiagoftsm@gmail.com>
2026-07-02 02:29:57 +0000

    abacus2: Multi (p1)

--- js/2836521c-6ddd-4fb9-a78d-b1b12db94883.js
+++ js/2836521c-6ddd-4fb9-a78d-b1b12db94883.js
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 function htLoadContent() {
-    htWriteNavigation();
-    $("#htChinaZhongguo").html(keywords[137]);
-    $("#htJapanNipponNihonKoku").html(keywords[139]);
+   htWriteNavigation();
+   $("#htChinaZhongguo").html(keywords[137]);
+   $("#htJapanNipponNihonKoku").html(keywords[139]);
 
-    return false;
+   htWriteMultiplicationTable("#mParent2", 2);
+
+   return false;
 }
