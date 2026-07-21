5877622c5ebb7fe311ad4df9a7a27a74e6bd3c54
History Tracers <historytracers@gmail.com>
2026-07-21 13:11:01 +0000

    yupana_sub: Adjust movements.

--- js/f17ec53f-af65-4708-bcc1-a140287d4dd9.js
+++ js/f17ec53f-af65-4708-bcc1-a140287d4dd9.js
@@ -56,103 +56,56 @@ function htLoadContent() {
     htWriteNavigation();
 
     $("#traineeUp6").on("click", function() {
-        htCleanYupanaDecimalValues('#yupana60', 1);
-        htFillYupanaDecimalValues('#yupana60', 3, 1, 'red_dot_right_up');
-        $("#leftHandImg6").attr("src","images/HistoryTracers/3Left_Hand_Small.png");
-        setTimeout(function() {
-            htCleanYupanaDecimalValues('#yupana60', 1);
-            htFillYupanaDecimalValues('#yupana60', 1, 1, 'red_dot_right_up');
-            htFillYupanaDecimalValues('#yupana60', 2, 1, 'blue_dot_right_bottom');
-            $("#leftHandImg6").attr("src","images/HistoryTracers/1Left_Hand_Small.png");
-            $("#rightHandImg6").attr("src","images/HistoryTracers/2Right_Hand_Small.png");
-            setTimeout(function() {
-                $("#leftHandImg6").attr("src","images/HistoryTracers/1Left_Hand_Small.png");
-                $("#rightHandImg6").attr("src","images/HistoryTracers/0Right_Hand_Small.png");
-                htInverseIskayMove(1, 0);
-                var totals = htSumYupanaVectors(local.inverseIskayLValues, local.inverseIskayRValues);
-                htFillYupanaDecimalValues('#yupana60', totals, 1, 'red_dot_right_up');
-            }, 1500);
-        }, 1500);
+        htCleanYupanaDecimalValues('#yupana60');
+        htFillYupanaDecimalValues('#yupana60', 2, 1, 'red_dot_right_up');
+        $("#leftHandImg6").attr("src","images/HistoryTracers/2Left_Hand_Small.png");
+        $("#rightHandImg6").attr("src","images/HistoryTracers/0Right_Hand_Small.png");
     });
 
     $("#traineeDown6").on("click", function() {
         $("#leftHandImg6").attr("src","images/HistoryTracers/3Left_Hand_Small.png");
-        $("#rightHandImg6").attr("src","images/HistoryTracers/0Right_Hand_Small.png");
-        htInverseIskayMove(3, 0);
+        $("#rightHandImg6").attr("src","images/HistoryTracers/1Right_Hand_Small.png");
+        htInverseIskayMove(3, 1);
         htWriteYupanaValuesOnHTMLTable('#vl', '#yupana60', local.inverseIskayLValues);
         htWriteYupanaValuesOnHTMLTable('#vr', '#yupana60', local.inverseIskayRValues);
     });
 
     $("#traineeUp7").on("click", function() {
-        htCleanYupanaDecimalValues('#yupana70', 1);
-        htFillYupanaDecimalValues('#yupana70', 5, 1, 'red_dot_right_up');
-        $("#leftHandImg7").attr("src","images/HistoryTracers/5Left_Hand_Small.png");
-        setTimeout(function() {
-            htCleanYupanaDecimalValues('#yupana70', 1);
-            htFillYupanaDecimalValues('#yupana70', 1, 1, 'red_dot_right_up');
-            htFillYupanaDecimalValues('#yupana70', 3, 1, 'blue_dot_right_bottom');
-            htFillYupanaDecimalValues('#yupana70', 1, 1, 'blue_dot_right_bottom');
-            $("#leftHandImg7").attr("src","images/HistoryTracers/1Left_Hand_Small.png");
-            $("#rightHandImg7").attr("src","images/HistoryTracers/4Right_Hand_Small.png");
-            setTimeout(function() {
-                $("#leftHandImg7").attr("src","images/HistoryTracers/1Left_Hand_Small.png");
-                $("#rightHandImg7").attr("src","images/HistoryTracers/3Right_Hand_Small.png");
-                htInverseKimsaMove(1, 3);
-                var totals = htSumYupanaVectors(local.inverseKimsaLValues, local.inverseKimsaRValues);
-                htFillYupanaDecimalValues('#yupana70', totals, 1, 'red_dot_right_up');
-            }, 1500);
-        }, 1500);
+        htCleanYupanaDecimalValues('#yupana70');
+        htFillYupanaDecimalValues('#yupana70', 4, 1, 'red_dot_right_up');
+        $("#leftHandImg7").attr("src","images/HistoryTracers/4Left_Hand_Small.png");
+        $("#rightHandImg7").attr("src","images/HistoryTracers/0Right_Hand_Small.png");
     });
 
     $("#traineeDown7").on("click", function() {
         $("#leftHandImg7").attr("src","images/HistoryTracers/5Left_Hand_Small.png");
-        $("#rightHandImg7").attr("src","images/HistoryTracers/0Right_Hand_Small.png");
-        htInverseKimsaMove(5, 0);
+        $("#rightHandImg7").attr("src","images/HistoryTracers/1Right_Hand_Small.png");
+        htInverseKimsaMove(5, 1);
         htWriteYupanaValuesOnHTMLTable('#vl', '#yupana70', local.inverseKimsaLValues);
         htWriteYupanaValuesOnHTMLTable('#vr', '#yupana70', local.inverseKimsaRValues);
     });
 
     $("#traineeUp8").on("click", function() {
-        htCleanYupanaDecimalValues('#yupana80', 2);
-        htFillYupanaDecimalValues('#yupana80', 10, 2, 'red_dot_right_up');
-        setTimeout(function() {
-            htCleanYupanaDecimalValues('#yupana80', 2);
-            htFillYupanaDecimalValues('#yupana80', 5, 2, 'red_dot_right_up');
-            $("#leftHandImg8").attr("src","images/HistoryTracers/5Left_Hand_Small.png");
-            setTimeout(function() {
-                htInversePisqaMove(5, 0);
-                var totals = htSumYupanaVectors(local.inversePisqaLValues, local.inversePisqaRValues);
-                htFillYupanaDecimalValues('#yupana80', totals, 2, 'red_dot_right_up');
-            }, 1500);
-        }, 1500);
+        htCleanYupanaDecimalValues('#yupana80');
+        htFillYupanaDecimalValues('#yupana80', 5, 2, 'red_dot_right_up');
+        $("#leftHandImg8").attr("src","images/HistoryTracers/5Left_Hand_Small.png");
+        $("#rightHandImg8").attr("src","images/HistoryTracers/0Right_Hand_Small.png");
     });
 
     $("#traineeDown8").on("click", function() {
         $("#leftHandImg8").attr("src","images/HistoryTracers/1Left_Hand_Small.png");
-        $("#rightHandImg8").attr("src","images/HistoryTracers/0Right_Hand_Small.png");
-        htInversePisqaMove(10, 0);
+        $("#rightHandImg8").attr("src","images/HistoryTracers/5Right_Hand_Small.png");
+        htInversePisqaMove(10, 5);
         htWriteYupanaValuesOnHTMLTable('#vl', '#yupana80', local.inversePisqaLValues);
         htWriteYupanaValuesOnHTMLTable('#vr', '#yupana80', local.inversePisqaRValues);
     });
 
     $("#traineeUp9").on("click", function() {
-        htCleanYupanaDecimalValues('#yupana90', 1);
-        htFillYupanaDecimalValues('#yupana90', 2, 1, 'red_dot_right_up');
-        $("#leftHandImg9").attr("src","images/HistoryTracers/2Left_Hand_Small.png");
-        setTimeout(function() {
-            htCleanYupanaDecimalValues('#yupana90', 1);
-            htFillYupanaDecimalValues('#yupana90', 1, 1, 'red_dot_right_up');
-            htFillYupanaDecimalValues('#yupana90', 1, 1, 'blue_dot_right_bottom');
-            $("#leftHandImg9").attr("src","images/HistoryTracers/1Left_Hand_Small.png");
-            $("#rightHandImg9").attr("src","images/HistoryTracers/1Right_Hand_Small.png");
-            setTimeout(function() {
-                $("#leftHandImg9").attr("src","images/HistoryTracers/1Left_Hand_Small.png");
-                $("#rightHandImg9").attr("src","images/HistoryTracers/0Right_Hand_Small.png");
-                htInversePichanaMove(1, 0);
-                var totals = htSumYupanaVectors(local.inversePichanaLValues, local.inversePichanaRValues);
-                htFillYupanaDecimalValues('#yupana90', totals, 1, 'red_dot_right_up');
-            }, 1500);
-        }, 1500);
+        $("#leftHandImg9").attr("src","images/HistoryTracers/1Left_Hand_Small.png");
+        $("#rightHandImg9").attr("src","images/HistoryTracers/0Right_Hand_Small.png");
+        htInversePichanaMove(1, 0);
+        var totals = htSumYupanaVectors(local.inversePichanaLValues, local.inversePichanaRValues);
+        htFillYupanaDecimalValues('#yupana90', totals, 1, 'red_dot_right_up');
     });
 
     $("#traineeDown9").on("click", function() {
@@ -164,19 +117,19 @@ function htLoadContent() {
     });
 
     if (local.sumFirstTime) {
-        htInverseIskayMove(3, 0);
-        htInverseKimsaMove(5, 0);
-        htInversePisqaMove(10, 0);
+        htInverseIskayMove(3, 1);
+        htInverseKimsaMove(5, 1);
+        htInversePisqaMove(10, 5);
         htInversePichanaMove(2, 0);
         local.sumFirstTime = false;
     }
 
     htSetImageSrc("leftHandImg6", "images/HistoryTracers/3Left_Hand_Small.png");
-    htSetImageSrc("rightHandImg6", "images/HistoryTracers/0Right_Hand_Small.png");
+    htSetImageSrc("rightHandImg6", "images/HistoryTracers/1Right_Hand_Small.png");
     htSetImageSrc("leftHandImg7", "images/HistoryTracers/5Left_Hand_Small.png");
-    htSetImageSrc("rightHandImg7", "images/HistoryTracers/0Right_Hand_Small.png");
+    htSetImageSrc("rightHandImg7", "images/HistoryTracers/1Right_Hand_Small.png");
     htSetImageSrc("leftHandImg8", "images/HistoryTracers/1Left_Hand_Small.png");
-    htSetImageSrc("rightHandImg8", "images/HistoryTracers/0Right_Hand_Small.png");
+    htSetImageSrc("rightHandImg8", "images/HistoryTracers/5Right_Hand_Small.png");
     htSetImageSrc("leftHandImg9", "images/HistoryTracers/2Left_Hand_Small.png");
     htSetImageSrc("rightHandImg9", "images/HistoryTracers/0Right_Hand_Small.png");
     return false;
