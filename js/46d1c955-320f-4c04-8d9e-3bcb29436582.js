5b9595fb399f3e78feea113d0e76143b24754b2e
History Tracers <historytracers@gmail.com>
2026-08-18 23:10:28 +0000

    mult_tables: Update time and index

--- js/46d1c955-320f-4c04-8d9e-3bcb29436582.js
+++ js/46d1c955-320f-4c04-8d9e-3bcb29436582.js
@@ -8,5 +8,7 @@ function htLoadContent() {
     htSetImageSrc("imgKingList", "images/Ashmolean/KingList.jpg")
     htSetImageSrc("imgHumanSpecies", "images/DonsMaps/1594.jpg")
     htSetImageSrc("imgUniverse", "images/ESA/Planck_history_of_Universe.jpg")
+    htSetImageSrc('imgGaiaStarFormation', 'images/ESA/Zoom_into_Gaia_s_star-formation_map_highlighted.jpg');
+    htSetImageSrc("imgALICE", "images/CERN/2ALICEimages.jpg");
     return false;
 }
