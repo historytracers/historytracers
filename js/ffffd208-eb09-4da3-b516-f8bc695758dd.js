a24d242e770042d9c78adb63b0d7253cf5328e46
History Tracers <historytracers@gmail.com>
2026-08-22 20:42:25 +0000

    equality: Fix https://github.com/historytracers/historytracers/pull/453#pullrequestreview-5000895733

--- js/ffffd208-eb09-4da3-b516-f8bc695758dd.js
+++ js/ffffd208-eb09-4da3-b516-f8bc695758dd.js
@@ -1,26 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-var localAnswerVector = undefined;
-
-function htLoadExercise() {
-    if (localAnswerVector == undefined) {
-        localAnswerVector = htLoadAnswersFromExercise();
-    } else {
-        htResetAnswers(localAnswerVector);
-    }
-
-    return false;
-}
-
-function htCheckAnswers()
-{
-    if (localAnswerVector != undefined) {
-        for (let i = 0; i < localAnswerVector.length; i++) {
-            htCheckExerciseAnswer("exercise"+i, localAnswerVector[i], "#answer"+i, "#explanation"+i);
-        }
-    }
-}
-
 function htLoadContent() {
     htWriteNavigation();
 
@@ -42,24 +21,5 @@ function htLoadContent() {
         shape.style.borderRadius = `${radius}%`;
     });
     
-    function morphToCircle() {
-        shape.style.borderRadius = '50%';
-        radiusSlider.value = 50;
-    }
-    
-    function morphToSquare() {
-        shape.style.borderRadius = '0%';
-        radiusSlider.value = 0;
-    }
-    
-    function toggleShape() {
-        const currentRadius = parseInt(radiusSlider.value);
-        if (currentRadius === 0) {
-            morphToCircle();
-        } else {
-            morphToSquare();
-        }
-    }
-
     return false;
 }
