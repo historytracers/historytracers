12c897c42a84ff16c7bd83c70b8a2b3649b5ac95
History Tracers <historytracers@gmail.com>
2026-08-27 18:26:09 +0000

    add_measure: End structure

--- js/be52fc9d-6701-4236-a4e3-22c05eb9324e.js
+++ js/be52fc9d-6701-4236-a4e3-22c05eb9324e.js
@@ -27,6 +27,8 @@ function htLoadContent() {
 		htSetImageSrc("mp", "images/MachuPicchu/MachuPicchu2.jpg");
 		htSetImageSrc("qp", "images/Caral/QuipuPanel.png");
 		htSetImageSrc("imgCW", "images/UNESCO/site_0438_0002.jpg");
+		htSetImageSrc('imgGeo24', 'images/Mapswire/mapswire-world-political-white-equal_earth_journey.png');
+    htSetImageSrc("imgWALL", "images/UNESCO/site_0364_0028-1000-750-20250313170037.jpg");
 
     return false;
 }
