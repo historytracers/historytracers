be3e6ee7457138c0d7a73cea6a9939af791ef28e
History Tracers <historytracers@gmail.com>
2026-09-12 02:06:19 +0000

    my_date: Finish new section

--- js/910c03b0-b736-4e15-9030-9cc95a835f09.js
+++ js/910c03b0-b736-4e15-9030-9cc95a835f09.js
@@ -2,6 +2,20 @@
 
 var localAnswerVector = undefined;
 
+function htFillRomanAbyaYalaTable() {
+    var current_time = Math.floor(Date.now()/1000);
+    var local_lang = $("#site_language").val();
+
+    $("#txtaymara").html(htConvertDate("aymara", local_lang, current_time, undefined, undefined));
+    $("#txtgregory").html(htConvertDate("gregory", local_lang, current_time, undefined, undefined));
+    $("#txtinca").html(htConvertDate("inca", local_lang, current_time, undefined, undefined));
+    $("#txthispanic").html(htConvertDate("hispanic", local_lang, current_time, undefined, undefined));
+    $("#txtjulian").html(htConvertDate("julian", local_lang, current_time, undefined, undefined));
+    $("#txtmapuche").html(htConvertDate("mapuche", local_lang, current_time, undefined, undefined));
+    $("#txtmesoamerican").html(htConvertDate("mesoamerican", local_lang, current_time, undefined, undefined));
+    $("#txtemesoamerican").html(htConvertDate("emesoamerican", local_lang, current_time, undefined, undefined));
+}
+
 function htLoadExercise() {
     if (localAnswerVector == undefined) {
         localAnswerVector = htLoadAnswersFromExercise();
@@ -24,6 +38,7 @@ function htCheckAnswers()
 function htLoadContent() {
     htWriteNavigation();
 		htSetImageSrc("imgCompostela", "images/Archive/historiadelasant04lpez_0376.jpg");
+		htFillRomanAbyaYalaTable();
 
     return false;
 }
